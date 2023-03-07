156b024b1c49e51b770d3c8091750bcc517d130a
Juan Barrionuevo <jottabarrionuevo@gmail.com>
2023-03-07 08:47:48 -0300

    se agrego bucles

---
+++ js/4-Bucles.js
@@ -0,0 +1,24 @@
+// while
+/*
+while (condicion logica){
+
+    todo el codigo que quiero repetir
+    alguna linea que haga que mi condicion logica deje de cumplir
+}
+*/
+
+let contador = 2;
+
+while(contador <= 10){
+
+
+    document.write("este es un parrafo N" +(contador)+"<br>");
+    //contador = contador +1
+    //contador ++; // contador = contador +1;
+    contador += 2; // contador= contador +2;
+
+}
+
+// do-while
+
+// for
